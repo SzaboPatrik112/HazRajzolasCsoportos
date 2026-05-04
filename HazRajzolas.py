69f634c493e91f19418f2b589465ec7bfd632dc9
SchrankSebestyen <schranksebestyen@gmail.com>
2026-05-04 09:09:10 +0200

    teljes haz de hibas

--- HazRajzolas.py
+++ HazRajzolas.py
@@ -3,11 +3,20 @@ t.speed(3)
 t.width(3)
 t.color("blue")
 
+#hazalja
+i=0
+t.left(180)
+while i<4:
+    t.forward(100)
+    t.right(90)
+    i+=1
+
 #haz teteje
 t.setheading(55)
 t.forward(60)
 t.setheading(-55)
 t.forward(60)
-t.done()
+
+
 
 t.done()
